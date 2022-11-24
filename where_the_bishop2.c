e29dff3ce5a25819822611838f4acd0f6db7cf9f
Tahsinah Banu Tisha <44545282+Tahsinah59@users.noreply.github.com>
2022-11-24 19:39:51 +0600

    Add files via upload

---
+++ where_the_bishop2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+	int T,i,j;
+	char a[10][10];
+	scanf("%d",&T);
+	while(T--)
+	{
+		for(i=1;i<=8;i++)
+			for(j=1;j<=8;j++)
+				scanf(" %c",&a[i][j]);
+
+
+	}
+}
+
